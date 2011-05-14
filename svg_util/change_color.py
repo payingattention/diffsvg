54a7f725de0d82118141612f4308294fb5caab05
Doug Philips <douglas.philips@gmail.com>
2011-05-14 01:40:47 -0400

    Use the ElementTree module parse function instead of the class's.
    Typical use of the ET abbreviation is for the module rather than the class.

--- svg_util/change_color.py
+++ svg_util/change_color.py
@@ -1,8 +1,7 @@
-from xml.etree.ElementTree import ElementTree as ET
+import xml.etree.ElementTree as ET
 from svg_util.etree import find, findall
 from svg_util.css import css_dict, css_string, set_property, get_property
-tree = ET()
-tree.parse("./tests/addabove/A.svg")
+tree = ET.parse("./tests/addabove/A.svg")
 root = tree.getroot()
 rect = find(root,"g/rect")
 set_property(rect,'fill',"#0000ff")
